25a016f5992418f541809a83495ec10e1661124c
ElkkuElias <elias.vuorinenkso@gmail.com>
2024-01-12 16:25:29 +0200

    mm

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.klinicsim {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
     requires java.desktop;
 
     exports view to javafx.graphics;
